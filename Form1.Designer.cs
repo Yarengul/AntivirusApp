71c5a7073337fdc0bd4e4df459ac0d48c1a066e6
Yarengül Kocaoğlu <yarenk777@gmail.com>
2026-01-04 22:43:58 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace AntivirusApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScanFile = new System.Windows.Forms.Button();
+            this.btnScanFolder = new System.Windows.Forms.Button();
+            this.lstResults = new System.Windows.Forms.ListBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.prgStatus = new System.Windows.Forms.ProgressBar();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnQuarantine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnScanFile
+            // 
+            this.btnScanFile.Location = new System.Drawing.Point(89, 51);
+            this.btnScanFile.Name = "btnScanFile";
+            this.btnScanFile.Size = new System.Drawing.Size(75, 23);
+            this.btnScanFile.TabIndex = 0;
+            this.btnScanFile.Text = "Dosya Tara";
+            this.btnScanFile.UseVisualStyleBackColor = true;
+            this.btnScanFile.Click += new System.EventHandler(this.btnScanFile_Click);
+            // 
+            // btnScanFolder
+            // 
+            this.btnScanFolder.Location = new System.Drawing.Point(89, 159);
+            this.btnScanFolder.Name = "btnScanFolder";
+            this.btnScanFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnScanFolder.TabIndex = 1;
+            this.btnScanFolder.Text = "Klasör Tara";
+            this.btnScanFolder.UseVisualStyleBackColor = true;
+            this.btnScanFolder.Click += new System.EventHandler(this.btnScanFolder_Click);
+            // 
+            // lstResults
+            // 
+            this.lstResults.FormattingEnabled = true;
+            this.lstResults.Location = new System.Drawing.Point(170, 51);
+            this.lstResults.Name = "lstResults";
+            this.lstResults.Size = new System.Drawing.Size(120, 95);
+            this.lstResults.TabIndex = 2;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(158, 202);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 3;
+            this.lblStatus.Text = "label1";
+            // 
+            // prgStatus
+            // 
+            this.prgStatus.Location = new System.Drawing.Point(190, 159);
+            this.prgStatus.Name = "prgStatus";
+            this.prgStatus.Size = new System.Drawing.Size(100, 23);
+            this.prgStatus.TabIndex = 4;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(313, 51);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(90, 23);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Seçileni Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnQuarantine
+            // 
+            this.btnQuarantine.Location = new System.Drawing.Point(313, 94);
+            this.btnQuarantine.Name = "btnQuarantine";
+            this.btnQuarantine.Size = new System.Drawing.Size(90, 23);
+            this.btnQuarantine.TabIndex = 6;
+            this.btnQuarantine.Text = "Karantinaya al";
+            this.btnQuarantine.UseVisualStyleBackColor = true;
+            this.btnQuarantine.Click += new System.EventHandler(this.btnQuarantine_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnQuarantine);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.prgStatus);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lstResults);
+            this.Controls.Add(this.btnScanFolder);
+            this.Controls.Add(this.btnScanFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScanFile;
+        private System.Windows.Forms.Button btnScanFolder;
+        private System.Windows.Forms.ListBox lstResults;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ProgressBar prgStatus;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnQuarantine;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography; // Şifreleme (Cryptography) kütüphanesi
+using System.IO; // Dosya Giriş/Çıkış (Input/Output) kütüphanesi
+
+namespace AntivirusApp
+{
+    public partial class Form1 : Form
+    {
+        // Basit bir virüs imza veritabanı (MD5 Hash Listesi)
+        // Buradaki hash değerleri temsilidir.
+        string[] virusSignatures = {
+            "44d88612fea8a8f36de82e1278abb02f",
+            "5d41402abc4b2a76b9719d911017c592"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // MD5 Hesaplama Fonksiyonu (MD5 Calculation Function)
+        public string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    var hash = md5.ComputeHash(stream);
+                    // Byte dizisini okunabilir metne (hexadecimal) çeviriyoruz
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
+        // Dosya Tara Butonu Tıklama Olayı (File Scan Button Click Event)
+        private void btnScanFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Taranacak Dosyayı Seçin (Select File to Scan)";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = ofd.FileName;
+                lblStatus.Text = "Taranıyor: " + Path.GetFileName(selectedFilePath);
+
+                try
+                {
+                    // Dosyanın hash değerini hesapla
+                    string fileHash = CalculateMD5(selectedFilePath);
+
+                    // Sonucu ListBox'a ekle
+                    lstResults.Items.Add("Dosya: " + selectedFilePath);
+                    lstResults.Items.Add("MD5: " + fileHash);
+
+                    // Virüs listesiyle karşılaştır
+                    bool isVirus = false;
+                    foreach (string signature in virusSignatures)
+                    {
+                        if (fileHash == signature)
+                        {
+                            isVirus = true;
+                            break;
+                        }
+                    }
+
+                    if (isVirus)
+                    {
+                        lstResults.Items.Add("DURUM: TEHLİKELİ! (VIRUS DETECTED)");
+                        MessageBox.Show("Tehlike Tespit Edildi!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                    else
+                    {
+                        lstResults.Items.Add("DURUM: GÜVENLİ (CLEAN)");
+                    }
+
+                    lstResults.Items.Add("------------------------------------");
+                    lblStatus.Text = "Tarama Tamamlandı.";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Hata oluştu: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnScanFolder_Click(object sender, EventArgs e)
+        {
+            // Klasör seçme penceresini aç (Folder Browser Dialog)
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Taramak istediğiniz klasörü seçin (Select Folder to Scan)";
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                string selectedPath = fbd.SelectedPath;
+                // Klasördeki tüm dosyaları al (Get all files in the directory)
+                string[] files = Directory.GetFiles(selectedPath, "*.*", SearchOption.AllDirectories);
+
+                // Progress bar ayarlarını yap (Setup progress bar)
+                prgStatus.Minimum = 0;
+                prgStatus.Maximum = files.Length;
+                prgStatus.Value = 0;
+
+                lstResults.Items.Clear(); // Önceki sonuçları temizle
+                lblStatus.Text = "Klasör taranıyor...";
+
+                int virusCount = 0;
+
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        string fileHash = CalculateMD5(file);
+                        bool isVirus = false;
+
+                        foreach (string signature in virusSignatures)
+                        {
+                            if (fileHash == signature)
+                            {
+                                isVirus = true;
+                                virusCount++;
+                                break;
+                            }
+                        }
+
+                        // Sadece virüs bulunursa veya her dosyayı göstermek isterseniz:
+                        if (isVirus)
+                        {
+                            lstResults.Items.Add("TEHLİKELİ: " + Path.GetFileName(file));
+                        }
+
+                        // İlerlemeyi güncelle
+                        prgStatus.Value++;
+                    }
+                    catch (Exception)
+                    {
+                        // Bazı dosyalara erişim yetkisi olmayabilir, onları atla
+                        continue;
+                    }
+                }
+
+                lblStatus.Text = $"Tarama bitti. {files.Length} dosya tarandı, {virusCount} virüs bulundu.";
+                MessageBox.Show($"Tarama tamamlandı!\nBulunan Virüs: {virusCount}", "Tarama Sonucu (Scan Result)");
+            }
+        }
+        // Silme İşlemi (Delete Action)
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstResults.SelectedItem != null)
+            {
+                string selectedLine = lstResults.SelectedItem.ToString();
+
+                // ListBox'taki metinden dosya yolunu ayıklamamız gerekebilir 
+                // Basitlik için sadece dosya yolu yazdırılan satırları seçtiğinizi varsayıyoruz
+                if (File.Exists(selectedLine))
+                {
+                    try
+                    {
+                        File.Delete(selectedLine);
+                        MessageBox.Show("Dosya başarıyla silindi.", "Başarılı (Success)");
+                        lstResults.Items.Remove(lstResults.SelectedItem);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Dosya silinemedi: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        // Karantina İşlemi (Quarantine Action)
+        private void btnQuarantine_Click(object sender, EventArgs e)
+        {
+            if (lstResults.SelectedItem != null)
+            {
+                string sourceFile = lstResults.SelectedItem.ToString();
+                string quarantinePath = Path.Combine(Application.StartupPath, "Quarantine");
+
+                if (!Directory.Exists(quarantinePath))
+                    Directory.CreateDirectory(quarantinePath);
+
+                if (File.Exists(sourceFile))
+                {
+                    try
+                    {
+                        string destFile = Path.Combine(quarantinePath, Path.GetFileName(sourceFile));
+                        File.Move(sourceFile, destFile); // Dosyayı taşı (Move)
+
+                        MessageBox.Show("Dosya Karantina klasörüne taşındı.", "Karantina");
+                        lstResults.Items.Remove(lstResults.SelectedItem);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Taşıma hatası: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+    }
+}
